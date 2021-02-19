66771d87e102fcf35f0c8176503ddb99c6673d58
emilybonar <emilykbonar@gmail.com>
2021-02-19 13:11:39 -0600

    Set up basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function App() {
-	return <div className=""></div>;
+	return (
+		<div className="flex w-screen h-screen gap-4 bg-blue-100">
+			<MusicalCard />
+			<MusicalCard />
+			<MusicalCard />
+		</div>
+	);
+}
+
+function MusicalCard() {
+	return <div className="bg-gray-100">Hello</div>;
 }
 
 export default App;
