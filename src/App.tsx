5386a1aa2014c58dd504969b94113df7899cf90e
emilybonar <emilykbonar@gmail.com>
2021-02-19 14:01:29 -0600

    added cards

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,44 @@ import React from "react";
 
 function App() {
 	return (
-		<div className="flex w-screen h-screen gap-4 bg-blue-100">
-			<MusicalCard />
-			<MusicalCard />
-			<MusicalCard />
+		<div className="grid w-screen h-screen bg-blue-100 place-items-center">
+			<div className="flex flex-wrap justify-center w-3/4 gap-4 h-3/4">
+				<MusicalCard
+					title="Hamilton"
+					image="https://austin.broadway.com/wp-content/uploads/2018/08/001_Show_Keyart_HAM-671x1065.jpg"
+					tracklist={[]}
+				/>
+				<MusicalCard
+					title="Hamilton"
+					image="https://austin.broadway.com/wp-content/uploads/2018/08/001_Show_Keyart_HAM-671x1065.jpg"
+					tracklist={[]}
+				/>
+				<MusicalCard
+					title="Hamilton"
+					image="https://austin.broadway.com/wp-content/uploads/2018/08/001_Show_Keyart_HAM-671x1065.jpg"
+					tracklist={[]}
+				/>
+			</div>
 		</div>
 	);
 }
 
-function MusicalCard() {
-	return <div className="bg-gray-100">Hello</div>;
+function MusicalCard(props: {
+	title: string;
+	image: string;
+	tracklist: string[];
+}) {
+	return (
+		<div className="w-40 h-64 overflow-hidden bg-gray-100 rounded shadow">
+			<img
+				className="object-cover object-center w-full h-3/4"
+				src={props.image}
+			/>
+			<div className="p-2">
+				<h1 className="text-lg font-semibold">{props.title}</h1>
+			</div>
+		</div>
+	);
 }
 
 export default App;
