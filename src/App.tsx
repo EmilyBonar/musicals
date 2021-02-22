47b4c1460edee72ee54444d1a4ae70479a491513
emilybonar <emilykbonar@gmail.com>
2021-02-22 08:56:22 -0600

    change background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
 	return (
-		<div className="grid w-screen h-screen bg-blue-100 place-items-center">
+		<div
+			className="grid w-screen h-screen bg-center bg-cover place-items-center"
+			style={{
+				backgroundImage:
+					"url(https://unsplash.com/photos/m3th3rIQ9-w/download?force=true&w=1920)",
+			}}
+		>
 			<div className="flex flex-wrap justify-center w-3/4 gap-4 h-3/4">
 				<MusicalCard
 					title="Hamilton"
 					image="https://austin.broadway.com/wp-content/uploads/2018/08/001_Show_Keyart_HAM-671x1065.jpg"
-					tracklist={[]}
+					tracklist={["test"]}
 				/>
 				<MusicalCard
 					title="Hamilton"
@@ -29,15 +35,29 @@ function MusicalCard(props: {
 	image: string;
 	tracklist: string[];
 }) {
+	const [flipped, setFlipped] = useState(false);
 	return (
-		<div className="w-40 h-64 overflow-hidden bg-gray-100 rounded shadow">
-			<img
-				className="object-cover object-center w-full h-3/4"
-				src={props.image}
-			/>
-			<div className="p-2">
-				<h1 className="text-lg font-semibold">{props.title}</h1>
+		<div className="flex flex-col w-64 overflow-hidden bg-gray-100 rounded shadow h-96">
+			<div className="w-full h-0">
+				<img
+					className="object-cover object-center w-full"
+					src={props.image}
+					onClick={() => setFlipped(!flipped)}
+				/>
 			</div>
+			{flipped && (
+				<div
+					className="h-full p-2 overflow-x-hidden overflow-y-auto bg-white bg-opacity-80"
+					onClick={() => setFlipped(!flipped)}
+				>
+					<h1 className="text-xl font-semibold text-center">{props.title}</h1>
+					<ol className="list-decimal list-inside">
+						{props.tracklist.map((track) => (
+							<li>{track}</li>
+						))}
+					</ol>
+				</div>
+			)}
 		</div>
 	);
 }
