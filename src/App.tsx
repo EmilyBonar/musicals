226bba6c5e2162de998ea3e7f99bcf372b946edc
emilybonar <emilykbonar@gmail.com>
2021-02-24 11:05:16 -0600

    Extracted components

--- src/App.tsx
+++ src/App.tsx
@@ -78,23 +78,41 @@ function MusicalCard(props: {
 				/>
 			</div>
 			{flipped && (
-				<div
-					className="h-full p-2 overflow-x-hidden overflow-y-auto bg-white cursor-auto bg-opacity-90"
+				<CardOverlay
 					onClick={() => setFlipped(!flipped)}
-				>
-					<h1 className="text-2xl font-semibold text-center">
-						{props.title} ({props.premiered.getFullYear()})
-					</h1>
-					<h2 className="text-lg text-center">{props.composers.join(", ")}</h2>
-					<ol className="pl-6 list-decimal">
-						{props.tracklist.map((track, index) => (
-							<li key={index} className="">
-								<TrackItem track={track} />
-							</li>
-						))}
-					</ol>
-				</div>
+					title={props.title}
+					premiered={props.premiered}
+					tracklist={props.tracklist}
+					composers={props.composers}
+				/>
 			)}
+		</div>
+	);
+}
+
+function CardOverlay(props: {
+	onClick: Function;
+	title: string;
+	premiered: Date;
+	tracklist: { title: string; link: string; length: number }[];
+	composers: string[];
+}) {
+	return (
+		<div
+			className="h-full p-2 overflow-x-hidden overflow-y-auto bg-white cursor-auto bg-opacity-90"
+			onClick={() => props.onClick()}
+		>
+			<h1 className="text-2xl font-semibold text-center">
+				{props.title} ({props.premiered.getFullYear()})
+			</h1>
+			<h2 className="text-lg text-center">{props.composers.join(", ")}</h2>
+			<ol className="pl-6 list-decimal">
+				{props.tracklist.map((track, index) => (
+					<li key={index} className="">
+						<TrackItem track={track} />
+					</li>
+				))}
+			</ol>
 		</div>
 	);
 }
@@ -102,17 +120,18 @@ function MusicalCard(props: {
 function TrackItem(props: {
 	track: { title: string; link: string; length: number };
 }) {
+	let timestamp =
+		Math.floor(props.track.length / 1000 / 60) +
+		":" +
+		Math.floor((props.track.length / 1000) % 60)
+			.toString()
+			.padStart(2, "0");
 	return (
 		<div className="inline-flex flex-row justify-between w-full">
 			<a className="cursor-pointer" href={props.track.link} target="_blank">
 				{props.track.title}
 			</a>
-			<p className="text-right">
-				{Math.floor(props.track.length / 1000 / 60)}:
-				{Math.floor((props.track.length / 1000) % 60)
-					.toString()
-					.padStart(2, "0")}
-			</p>
+			<p className="text-right">{timestamp}</p>
 		</div>
 	);
 }
