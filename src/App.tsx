da64d9229380ef6f6f06f766369e3574fe22a630
emilybonar <emilykbonar@gmail.com>
2021-02-25 16:56:54 -0600

    Maybe this will work

--- src/App.tsx
+++ src/App.tsx
@@ -108,7 +108,7 @@ function HeaderBar(props: { onChange: Function }) {
 function FooterBar() {
 	return (
 		<footer className="flex justify-center w-full p-2 bg-black bg-opacity-90">
-			<form name="suggestions">
+			<form name="suggestions" method="post">
 				<input
 					className="p-2 m-1 text-gray-200 bg-gray-900 rounded"
 					placeholder="Suggest a new musical title"
